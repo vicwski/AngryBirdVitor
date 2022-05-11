9a95692e727e0ced25dec77649047d86f0da608a
vicwski <vic.gajewski@gmail.com>
2022-05-11 15:37:52 -0300

    aula c31

--- script.js
+++ script.js
@@ -21,9 +21,29 @@ console.log(object)
 //Reatribuindo o mesmo objeto indefinido para nulo
 //Nulo
 //Null:  nada ou vazio
-// object = null
-// console.log(object)
+object = null
+console.log(object)
 
 //--------------------------------------------------------------------------------------------------
 
-//Matrizes = Arrays = lista
+//Matrizes = Arrays = lista []
+
+
+var arr = [23, "oi", bool, [34, 87]]
+console.log(arr)
+console.log(arr.length)
+
+console.log(arr[3][1])
+
+arr.push("olá")
+console.log(arr)
+console.log(arr.length)
+
+arr.pop()
+console.log(arr)
+console.log(arr.length)
+
+
+arr = []
+console.log(arr)
+console.log(arr.length)
