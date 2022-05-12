afcf62217ff317b7fb8dd2c565f3e5cc09d9b4da
VicGA <vic.gajewski@gmail.com>
2022-05-12 18:45:54 -0300

    commmit

--- Pig.js
+++ Pig.js
@@ -6,7 +6,6 @@ class Pig extends BaseClass {
   }
 
   display() {
-    console.log(this.body.speed)
     if (this.body.speed < 3) {
       super.display()
     } else {
---
+++ script.js
@@ -0,0 +1,29 @@
+//Matrizes ~~~ Dados
+//Exemplos sobre os diferentes tipos de dados em JavaScript
+
+//String
+var string = 'Isso é uma string'
+console.log(string)
+
+//Número
+var num = 100
+console.log(num)
+
+//Boleano (verdadeiro ou falso)
+var bool = true
+console.log(bool)
+
+//Indefinido
+//Undefined: nenhum valor foi atribuido a uma variável
+var object
+console.log(object)
+
+//Reatribuindo o mesmo objeto indefinido para nulo
+//Nulo
+//Null:  nada ou vazio
+// object = null
+// console.log(object)
+
+//--------------------------------------------------------------------------------------------------
+
+//Matrizes = Arrays = listas
